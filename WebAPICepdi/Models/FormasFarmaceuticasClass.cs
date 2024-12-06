5940dbc0c56a882c82af616b354cf88adb945f09
jagoraxr <jagoraxr.moucha@gmail.com>
2024-12-06 13:57:44 -0600

    Add validate format password --> create and update

--- WebAPICepdi/Models/FormasFarmaceuticasClass.cs
+++ WebAPICepdi/Models/FormasFarmaceuticasClass.cs
@@ -1,7 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WebAPICepdi.Models
 {
@@ -9,6 +8,7 @@ namespace WebAPICepdi.Models
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int idFormaFarmaceutica { get; set; }
         public string nombre { get; set; } = string.Empty;
         public int? habilitado { get; set; }
         public ICollection<Medicamentos> Medicamentoss { get; set; }
